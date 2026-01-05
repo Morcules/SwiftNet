7028d54a3a8169f7b334900a3e3b5b67b6a2e980
Sandeep <7006205280dk@gmail.com>
2026-01-05 12:18:44 +0530

    Refactor: Remove sender_address from SwiftNet send_packet API and update headers

---
+++ tests/integration_tests/build/CMakeFiles/run_tests.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "/Users/dev/Repositories/SwiftNet/tests/integration_tests/output/run_tests"
+  "/Users/dev/Repositories/SwiftNet/tests/integration_tests/output/run_tests.pdb"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/making_request.c.o"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/making_request.c.o.d"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/run_tests.c.o"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/run_tests.c.o.d"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/sending_packet.c.o"
+  "CMakeFiles/run_tests.dir/Users/dev/Repositories/SwiftNet/tests/integration_tests/src/sending_packet.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/run_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
