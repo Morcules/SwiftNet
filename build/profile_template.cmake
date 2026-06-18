9d4a9cd8ba1faf91b26a6bb85275f28d6fbf77d2
morcules <morcules@protonmail.com>
2026-06-18 10:06:24 +0200

    Updated workflow file and template

--- build/profile_template.cmake
+++ build/profile_template.cmake
@@ -1,9 +1,6 @@
-set(CMAKE_C_FLAGS "-O3")
-set(CMAKE_CXX_FLAGS "-O3")
-
 add_compile_definitions(
     # SWIFT_NET_DISABLE_REQUESTS # Compile source without request features (make_request, make_response)
-    SWIFT_NET_MEMORY_USAGE= # Multiplier of memory preallocated
+    SWIFT_NET_MEMORY_USAGE=5 # Multiplier of memory preallocated
 )
 
 set(SANITIZER "none") # (thread, address, undefined, none (no sanitizer))
